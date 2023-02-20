75e647e00c1daedf91db04293edd3f97ae10b0b2
Jeremy <vaugoyeau.jeremy@gmail.com>
2023-02-20 15:06:28 +0100

    ajout un peu de style

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,46 @@
-document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-    let addForm = document.getElementById("add-pizza");
-    let pizzaList = document.querySelector(".pizzas");
-    let addName = document.getElementById("nomPizza");
-    let addIngredients = document.getElementById("ingredients");
-  
-    addForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      if (isValid(addName.value) && isValid(addIngredients.value)) {
-          let newPizza = document.createElement('div');
-          newPizza.classList.add('pizza');
-          newPizza.innerHTML = `
-          <div class="pizza">
-          <p>${capitalizeFisrtLetter(addName.value)}</p>
-          <p>${formatIngredients(addIngredients.value)}</p>
-          </div>`;
-          
-          pizzaList.appendChild(newPizza);
-         resetInput(addName);
-         resetInput(addIngredients);
-      } else {
-          let errorContainer = document.getElementById('errorContainer');
-          errorContainer.textContent = "la saisie n'est pas valide";
-          setTimeout(() => errorContainer.textContent = '', 2000);
-      }
-        
-    });
+  // Cordova is now initialized. Have fun!
+  let addForm = document.getElementById("add-pizza");
+  let pizzaList = document.querySelector(".pizzas");
+  let addName = document.getElementById("nomPizza");
+  let addIngredients = document.getElementById("ingredients");
 
-    function resetInput(input){
-        input.value= "";
-    }
-    function isValid(value) {
-        return value && value.length > 2 && value.length < 50;
-    }
+  addForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (isValid(addName.value) && isValid(addIngredients.value)) {
+      let newPizza = document.createElement("div");
+      newPizza.classList.add("pizza");
+      newPizza.innerHTML = `
+        <ion-item>
+            <ion-label>
+                <h2>${capitalizeFisrtLetter(addName.value)}</h2>
+                <p>${formatIngredients(addIngredients.value)}</p>
+            </ion-label>
+        </ion-item>`;
 
-    function formatIngredients(value){
-        return "("+ value.trim().replaceAll(" ", ", ")+")"
+      pizzaList.appendChild(newPizza);
+      resetInput(addName);
+      resetInput(addIngredients);
+    } else {
+      let errorContainer = document.getElementById("errorContainer");
+      errorContainer.textContent = "la saisie n'est pas valide";
+      setTimeout(() => (errorContainer.textContent = ""), 2000);
     }
-    function capitalizeFisrtLetter(value){
-        return value.trim().slice(0, 1).toUpperCase() + value.trim().slice(1);
-    }
+  });
+
+  function resetInput(input) {
+    input.value = "";
+  }
+  function isValid(value) {
+    return value && value.length > 2 && value.length < 50;
+  }
+
+  function formatIngredients(value) {
+    return "(" + value.trim().replaceAll(" ", ", ") + ")";
+  }
+  function capitalizeFisrtLetter(value) {
+    return value.trim().slice(0, 1).toUpperCase() + value.trim().slice(1);
+  }
 }
-
