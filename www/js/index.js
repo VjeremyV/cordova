74b0b3b4be14ca76e9574b31a715dcc9ad2b2cea
Jeremy <vaugoyeau.jeremy@gmail.com>
2023-02-20 10:49:02 +0100

    ajout fonction anonyme pour encapsuler le code js + un peu de style

--- www/js/index.js
+++ www/js/index.js
@@ -7,22 +7,23 @@
 //     document.getElementById('deviceready').classList.add('ready');
 // }
 
-
-  let addForm = document.getElementById("add-pizza");
-  let pizzaList = document.querySelector(".pizzas");
-  let addName = document.getElementById("nomPizza");
-  let addIngredients = document.getElementById("ingredients");
-
-  addForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let newPizza = document.createElement('div');
-    newPizza.classList.add('pizza');
-    newPizza.innerHTML = `
-            <div class="pizza">
-                <p>${addName.value}</p>
-                <p>${addIngredients.value}</p>
-            </div>`;
-
-    pizzaList.appendChild(newPizza);
-  });
+(()=> {
+    let addForm = document.getElementById("add-pizza");
+    let pizzaList = document.querySelector(".pizzas");
+    let addName = document.getElementById("nomPizza");
+    let addIngredients = document.getElementById("ingredients");
+  
+    addForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      let newPizza = document.createElement('div');
+      newPizza.classList.add('pizza');
+      newPizza.innerHTML = `
+              <div class="pizza">
+                  <p>${addName.value}</p>
+                  <p>${addIngredients.value}</p>
+              </div>`;
+  
+      pizzaList.appendChild(newPizza);
+    });
+})()
 
