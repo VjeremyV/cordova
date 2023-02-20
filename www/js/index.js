9561b624681fe4c207bc09562e6d9b6413e63001
Jeremy <vaugoyeau.jeremy@gmail.com>
2023-02-20 10:41:34 +0100

    ajout formulaire pour ajouter une pizza

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,28 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// document.addEventListener('deviceready', onDeviceReady, false);
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+// function onDeviceReady() {
+//     // Cordova is now initialized. Have fun!
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+//     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+//     document.getElementById('deviceready').classList.add('ready');
+// }
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
+
+  let addForm = document.getElementById("add-pizza");
+  let pizzaList = document.querySelector(".pizzas");
+  let addName = document.getElementById("nomPizza");
+  let addIngredients = document.getElementById("ingredients");
+
+  addForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newPizza = document.createElement('div');
+    newPizza.classList.add('pizza');
+    newPizza.innerHTML = `
+            <div class="pizza">
+                <p>${addName.value}</p>
+                <p>${addIngredients.value}</p>
+            </div>`;
+
+    pizzaList.appendChild(newPizza);
+  });
+
